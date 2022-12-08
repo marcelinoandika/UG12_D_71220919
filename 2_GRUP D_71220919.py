eeafcf4b124cba27d508ea3839f5c548c73bbee9
marcelinoandika <117901348+marcelinoandika@users.noreply.github.com>
2022-12-08 16:29:19 +0700

    Update 2_GRUP D_71220919.py

--- 2_GRUP D_71220919.py
+++ 2_GRUP D_71220919.py
@@ -1,12 +1,18 @@
 print ("~~~~~ Table Matematika Nich ~~~~~")
 print ("Pilihan Model Matematika ")
-m = print ("1. Perkalian")
-p = print ("2. Pembagian")
+print ("1. Perkalian")
+print ("2. Pembagian")
 x = int(input("Masukkan model matematika yang diinginkan 1/2 :"))
 y = int(input("Menampilkan table matematika dari angka: "))
-for i in range(1, 11):
-    print(y, 'x', i, '=', y*i)
 
-if x== 1 :
-    jumlah = x*y
-    print(y, 'x', i, '=', y*i)
+if x == 1 :
+    for i in range(1, 11):
+        print(y, 'x', i, '=', y*i)
+elif x == 2:
+    for i in range(50, 66):
+        print(i, ':', y, '=', i/y)
+else:
+    print("Pilihan tidak tersedia, jangan ngasal!")
+
+    
+

c74c9041c8050625a39bc6efcbaad841c92a221d
marcelinoandika <117901348+marcelinoandika@users.noreply.github.com>
2022-12-08 16:14:14 +0700

    Update 2_GRUP D_71220919.py

--- 2_GRUP D_71220919.py
+++ 2_GRUP D_71220919.py
@@ -1,6 +1,12 @@
 print ("~~~~~ Table Matematika Nich ~~~~~")
 print ("Pilihan Model Matematika ")
-print ("1. Perkalian")
-print ("2. Pembagian")
-print ("Masukkan model matematika yang diinginkan 1/2 :")
-print ("Menampilkan table matematika dari angka: ")
+m = print ("1. Perkalian")
+p = print ("2. Pembagian")
+x = int(input("Masukkan model matematika yang diinginkan 1/2 :"))
+y = int(input("Menampilkan table matematika dari angka: "))
+for i in range(1, 11):
+    print(y, 'x', i, '=', y*i)
+
+if x== 1 :
+    jumlah = x*y
+    print(y, 'x', i, '=', y*i)
